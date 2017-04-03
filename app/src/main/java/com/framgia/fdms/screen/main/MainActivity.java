3ad4d85411673c8751c91f14bb54fadb12b9e2b7
huy <nguyenhuy95dn@gmail.com>
2017-04-03 14:01:50 +0700

    main ui

---
+++ app/src/main/java/com/framgia/fdms/screen/main/MainActivity.java
@@ -0,0 +1,39 @@
+package com.framgia.fdms.screen.main;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.framgia.fdms.R;
+import com.framgia.fdms.databinding.ActivityMainBinding;
+
+/**
+ * Main Screen.
+ */
+public class MainActivity extends AppCompatActivity {
+    private MainContract.ViewModel mViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mViewModel = new MainViewModel();
+        MainContract.Presenter presenter =
+            new MainPresenter(mViewModel);
+        mViewModel.setPresenter(presenter);
+        ActivityMainBinding binding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setViewModel((MainViewModel) mViewModel);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mViewModel.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mViewModel.onStop();
+        super.onStop();
+    }
+}
