d2e722dec27505fb8de1add93b2bc7cf9601c18a
huy <nguyenhuy95dn@gmail.com>
2017-04-05 10:21:48 +0700

    feature logic device detail

--- app/src/main/java/com/framgia/fdms/screen/detail/DetailActivity.java
+++ app/src/main/java/com/framgia/fdms/screen/detail/DetailActivity.java
@@ -1,9 +1,12 @@
 package com.framgia.fdms.screen.detail;
 
+import android.content.Context;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import com.framgia.fdms.R;
+import com.framgia.fdms.data.model.Device;
 import com.framgia.fdms.databinding.DetailDataBinding;
 
 /**
@@ -11,13 +14,23 @@ import com.framgia.fdms.databinding.DetailDataBinding;
  */
 public class DetailActivity extends AppCompatActivity {
 
+    private static final String EXTRA_DEVICE = "EXTRA_DEVICE";
+
     private DetailContract.ViewModel mViewModel;
+
+    public static Intent getDeviceIntent(Context context, Device device) {
+        Intent intent = new Intent(context, DetailActivity.class);
+        intent.putExtra(EXTRA_DEVICE, device);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mViewModel = new DetailViewModel();
+        Device device = (Device) getIntent().getSerializableExtra(EXTRA_DEVICE);
+        setTitle(device.getName());
+        mViewModel = new DetailViewModel(this, device);
 
         DetailContract.Presenter presenter = new DetailPresenter(mViewModel);
         mViewModel.setPresenter(presenter);
--- app/src/main/java/com/framgia/fdms/screen/detail/DetailViewModel.java
+++ app/src/main/java/com/framgia/fdms/screen/detail/DetailViewModel.java
@@ -1,14 +1,23 @@
 package com.framgia.fdms.screen.detail;
 
+import android.content.Context;
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import com.framgia.fdms.data.model.Device;
+
 /**
  * Exposes the data to be used in the Detail screen.
  */
 
-public class DetailViewModel implements DetailContract.ViewModel {
+public class DetailViewModel extends BaseObservable implements DetailContract.ViewModel {
 
+    private Context mContext;
     private DetailContract.Presenter mPresenter;
+    private Device mDevice;
 
-    public DetailViewModel() {
+    public DetailViewModel(Context context, Device device) {
+        mContext = context;
+        mDevice = device;
     }
 
     @Override
@@ -24,5 +33,25 @@ public class DetailViewModel implements DetailContract.ViewModel {
     @Override
     public void setPresenter(DetailContract.Presenter presenter) {
         mPresenter = presenter;
+    }
+
+    @Bindable
+    public String getName() {
+        return mDevice.getName();
+    }
+
+    @Bindable
+    public String getCategory() {
+        return mDevice.getCategory();
+    }
+
+    @Bindable
+    public String getDescription() {
+        return mDevice.getDescription();
+    }
+
+    @Bindable
+    public String getImageUrl() {
+        return mDevice.getImage();
     }
 }
