d161f24f13e7ba67c1cb245946eeaab18ebabf35
ongnau <beepig025@gmail.com>
2017-05-05 09:51:52 +0700

    qr code

--- app/src/main/java/com/framgia/fdms/FDMSApplication.java
+++ app/src/main/java/com/framgia/fdms/FDMSApplication.java
@@ -7,9 +7,16 @@ import com.framgia.fdms.data.source.api.service.FDMSServiceClient;
  * Created by Age on 4/3/2017.
  */
 public class FDMSApplication extends Application {
+    private static FDMSApplication sInstant;
+
     @Override
     public void onCreate() {
         super.onCreate();
         FDMSServiceClient.initialize(this);
+        sInstant = this;
+    }
+
+    public static FDMSApplication getInstant() {
+        return sInstant;
     }
 }
