5512ff76eaca7120c84f6bb89e8726f6d2982cb8
ToanDoan <toandoan.coder@gmail.c>
2017-05-15 10:34:46 +0700

    add unswipe viewpager

--- app/src/main/java/com/framgia/fdms/utils/Utils.java
+++ app/src/main/java/com/framgia/fdms/utils/Utils.java
@@ -3,6 +3,8 @@ package com.framgia.fdms.utils;
 import com.framgia.fdms.data.model.Respone;
 import rx.Observable;
 
+import static com.framgia.fdms.utils.Constant.PERCENT;
+
 /**
  * Created by MyPC on 05/05/2017.
  */
@@ -16,5 +18,15 @@ public class Utils {
         } else {
             return Observable.just(listRespone.getData());
         }
+    }
+
+    public static String getStringPercent(int count, int total) {
+        float percent;
+        if (total == 0) {
+            percent = 0;
+        } else {
+            percent = (float) count / total * 100f;
+        }
+        return String.format("%.1f", percent) + PERCENT;
     }
 }
