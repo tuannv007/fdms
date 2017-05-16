3e8a58dc01279b5c45f5d16500f7de5fcb6bca4f
datchiz <datchiz5@gmail.com>
2017-05-16 10:33:18 +0700

    ui dashboard

--- app/src/main/java/com/framgia/fdms/screen/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/framgia/fdms/screen/dashboard/DashboardViewModel.java
@@ -1,20 +1,30 @@
 package com.framgia.fdms.screen.dashboard;
 
 import android.content.Context;
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
 import android.support.annotation.IntDef;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import com.framgia.fdms.BR;
 
 /**
  * Exposes the data to be used in the Dashboard screen.
  */
 
-public class DashboardViewModel implements DashboardContract.ViewModel {
+public class DashboardViewModel extends BaseObservable implements DashboardContract.ViewModel {
 
     private DashboardContract.Presenter mPresenter;
     private ViewPagerAdapter mPagerAdapter;
+    private int mTab = Tab.TAB_DEVIVE_DASH_BOARD;
 
     public DashboardViewModel(DashboardFragment fragment) {
         mPagerAdapter = new ViewPagerAdapter(fragment.getChildFragmentManager());
+    }
+
+    public void onDirectChildTab(@Tab int tab, ViewPager viewPager) {
+        if (mPagerAdapter == null) return;
+        viewPager.setCurrentItem(tab);
     }
 
     @Override
@@ -34,6 +44,16 @@ public class DashboardViewModel implements DashboardContract.ViewModel {
 
     public ViewPagerAdapter getPagerAdapter() {
         return mPagerAdapter;
+    }
+
+    @Bindable
+    public int getTab() {
+        return mTab;
+    }
+
+    public void setTab(int tab) {
+        mTab = tab;
+        notifyPropertyChanged(BR.tab);
     }
 
     @IntDef({
