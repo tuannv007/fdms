ead33960e34fd33ce781ca09c28c874e7c4b9022
ToanDoan <toandoan.coder@gmail.c>
2017-05-23 09:44:50 +0700

    add device using history

--- app/src/main/java/com/framgia/fdms/screen/main/MainViewModel.java
+++ app/src/main/java/com/framgia/fdms/screen/main/MainViewModel.java
@@ -6,13 +6,10 @@ import android.databinding.BaseObservable;
 import android.databinding.Bindable;
 import android.databinding.ObservableField;
 import android.support.v7.widget.RecyclerView;
-import android.widget.Toast;
 import android.view.View;
 import com.framgia.fdms.BaseRecyclerViewAdapter;
-import com.framgia.fdms.R;
 import com.framgia.fdms.data.model.Device;
 import com.framgia.fdms.screen.detail.DetailActivity;
-import com.framgia.fdms.screen.device.DeviceActivity;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -78,9 +75,5 @@ public class MainViewModel extends BaseObservable implements MainContract.ViewMo
     @Override
     public void hideProgressbar() {
         progressBarVisibility.set(View.GONE);
-    }
-
-    public void onRegisterDeviceClick() {
-        mContext.startActivity(new Intent(mContext, DeviceActivity.class));
     }
 }
