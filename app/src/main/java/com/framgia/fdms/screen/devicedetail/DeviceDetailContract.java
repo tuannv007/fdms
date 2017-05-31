19f6f1b4253ec57d70da1b95675c02019d844e7e
datchiz <datchiz5@gmail.com>
2017-05-31 15:50:43 +0700

    fix image device detail

--- app/src/main/java/com/framgia/fdms/screen/devicedetail/DeviceDetailContract.java
+++ app/src/main/java/com/framgia/fdms/screen/devicedetail/DeviceDetailContract.java
@@ -2,6 +2,7 @@ package com.framgia.fdms.screen.devicedetail;
 
 import com.framgia.fdms.BasePresenter;
 import com.framgia.fdms.BaseViewModel;
+import com.framgia.fdms.data.model.Device;
 
 /**
  * This specifies the contract between the view and the presenter.
@@ -12,11 +13,14 @@ interface DeviceDetailContract {
      */
     interface ViewModel extends BaseViewModel<Presenter> {
         void onEditDevice();
+
+        void onGetDeviceSuccess(Device device);
     }
 
     /**
      * Presenter.
      */
     interface Presenter extends BasePresenter {
+        void getDevice(int deviceId);
     }
 }
